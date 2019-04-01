6ff989c33e6dddfe8841e6191a14313ef4e2cb58
Prakash Venkat <venkatraman.prakash@gmail.com>
2019-03-31 19:50:40 -0700

    initial commit

---
+++ blog/api/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_url = models.URLField()
