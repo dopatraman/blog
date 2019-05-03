ece3e015120ee64757e11a78286c122155ba20f6
Prakash Venkat <venkatraman.prakash@gmail.com>
2019-05-03 13:48:35 -0700

    Notes on WIP

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# NOTE: A Controller class  (or something) can export all of these paths. Will
+# give the chance for dependency injection (re: storage service)
 urlpatterns = [
     path("", views.get_all_posts, name="get_all_posts"),
     path("<int:post_id>", views.get_post, name="get_post"),
