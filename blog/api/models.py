81da5574f59b72ed2b0b424b9a5fe444f8219546
Prakash Venkat <venkatraman.prakash@gmail.com>
2019-05-07 13:02:15 -0700

    json repr method

--- blog/api/models.py
+++ blog/api/models.py
@@ -6,6 +6,18 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __json__(self):
+        fields = self._meta.fields
+        json = {}
+        for field in fields:
+            key = field.name
+            value = self.__getattribute__(key)
+            json[key] = value
+        return json
+
+    def json(self):
+        return self.__json__()
+
     class Meta:
         abstract = True
 
