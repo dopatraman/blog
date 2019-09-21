a1045ed32f78f3f01cef529469f408c0c892b4e1
Prakash Venkatraman <venkatraman.prakash@gmail.com>
2019-09-21 15:29:40 -0700

    Feature/create post ui (#10)
    * Basic Post Editor UI
    
    * Stopped border jumping
    
    * Panel buttons
    
    * Restyle sidebar
    
    * id for create button

--- api/assets/js/lib/ui.js
+++ api/assets/js/lib/ui.js
@@ -3,9 +3,11 @@ import ApiClient from "./apiClient";
 window.apiClient = new ApiClient();
 window.onload = function() {
   const postButton = document.getElementById("create-post");
-  postButton.addEventListener("click", (evt) => {
-    const title = document.getElementById("title").value;
-    const content = document.getElementById("content").value;
-    apiClient.createPost(title, content);
-  })
+  if (postButton !== null) {
+    postButton.addEventListener("click", (evt) => {
+      const title = document.getElementById("title").value;
+      const content = document.getElementById("content").value;
+      apiClient.createPost(title, content);
+    })
+  }
 }
