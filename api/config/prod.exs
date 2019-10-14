f0054bb10fd7329a87514d006a9b9c65b1a1cd12
Prakash Venkatraman <venkatraman.prakash@gmail.com>
2019-10-13 23:27:28 -0700

    Feature/deployment (#12)
    * Add deployment values to prod config file
    
    * Test docker build working
    
    * Docker build and release generation

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -55,7 +55,8 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :api, ApiWeb.Endpoint, server: true
+config :api, ApiWeb.Endpoint, server: true
+
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
