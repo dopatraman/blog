e647dba79fbd5cdde0e5c9e25a9364e860148308
Prakash Venkatraman <venkatraman.prakash@gmail.com>
2019-10-17 22:27:02 -0700

    Feature/release migrations (#14)
    * WIP release migration module
    
    * Release tasks for migration and seeding db

---
+++ api/lib/release_tasks.ex
@@ -0,0 +1,24 @@
+defmodule ReleaseTasks do
+  def migrate do
+    {:ok, _} = Application.ensure_all_started(:api)
+    Ecto.Migrator.run(
+      Api.Repo,
+      path("priv/repo/migrations"),
+      :up,
+      all: true)
+
+    # Seed db
+    seed_database()
+
+    # Close process
+    :init.stop()
+  end
+
+  defp seed_database() do
+    Code.require_file(path("priv/repo/seeds.exs"))
+  end
+
+  defp path(path_string) do
+    Application.app_dir(:api, path_string)
+  end
+end
