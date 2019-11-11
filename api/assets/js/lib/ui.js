884cf3b37cde15c306c177de78f38d7dc93cc9c6
Prakash Venkatraman <venkatraman.prakash@gmail.com>
2019-11-11 10:51:55 -0800

    Add action to login button (#18)

--- api/assets/js/lib/ui.js
+++ api/assets/js/lib/ui.js
@@ -1,13 +1,22 @@
 import ApiClient from "./apiClient";
 
-window.apiClient = new ApiClient();
 window.onload = function() {
+  const apiClient = new ApiClient();
   const postButton = document.getElementById("create-post");
   if (postButton !== null) {
     postButton.addEventListener("click", (evt) => {
       const title = document.getElementById("title").value;
       const content = document.getElementById("content").value;
       apiClient.createPost(title, content);
+    })
+  }
+
+  const loginButton = document.getElementById("login");
+  const usernameField = document.getElementById("username");
+  const passwordField = document.getElementById("password");
+  if (loginButton !== null && usernameField !== null && passwordField !== null) {
+    loginButton.addEventListener("click", (evt) => {
+      apiClient.login(usernameField.value, passwordField.value);
     })
   }
 }
