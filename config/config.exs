62c144f05e4742a131cebce1bc902db3e90a2deb
Prakash Venkat <venkatraman.prakash@gmail.com>
2020-10-11 16:58:39 -0700

    Added port to config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :blog,
+  port: 8000,
   post_repo_url: "https://github.com/dopatraman/blog-posts.git",
   local_content_dir: Path.expand("./.content")
 
--- lib/application.ex
+++ lib/application.ex
@@ -4,16 +4,18 @@ defmodule Blog.Application do
   use Application
 
   def start(_type, _args) do
+    port = Application.get_env(:blog, :port)
+
     children = [
       Plug.Cowboy.child_spec(
         scheme: :http,
         plug: Blog.Web.Endpoint,
-        options: [port: 8000]
+        options: [port: port]
       )
     ]
 
     opts = [strategy: :one_for_one, name: Blog.Supervisor]
-    IO.puts("Starting...")
+    IO.puts("Starting on port #{port}...")
     Supervisor.start_link(children, opts) |> IO.inspect()
   end
 end
