c5cdb2305b8d9e609d734ea6f0adf61e07a14762
Prakash Venkat <venkatraman.prakash@gmail.com>
2020-10-17 23:53:23 -0700

    Template directory reference

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@ import Config
 config :blog,
   port: 8000,
   post_repo_url: "https://github.com/dopatraman/blog-posts.git",
-  local_content_dir: Path.expand("./.content")
+  local_content_dir: Path.expand("./.content"),
+  template_dir: Path.expand("./lib/web/views")
 
 import_config "#{Mix.env()}.exs"
